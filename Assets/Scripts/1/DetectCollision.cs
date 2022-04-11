793ce267b29e815006e226b9d3a7e6af38471549
JDJD6 <89411423+JDJD6@users.noreply.github.com>
2022-04-10 22:14:33 -0700

    Game over add

--- Assets/Scripts/1/DetectCollision.cs
+++ Assets/Scripts/1/DetectCollision.cs
@@ -4,9 +4,26 @@ using UnityEngine;
 
 public class DetectCollision : MonoBehaviour
 {
+    public bool gameOver = false;
+    
     private void OnTriggerEnter(Collider other)
     {
         Score1.scoreValue += 50;
         Destroy(gameObject);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == ("Gameover"))
+        {
+            Debug.Log("Game Over!");
+            gameOver = true;
+
+            GameObject.Find("Spawn Manager").GetComponent<SpawnManager>().enabled = false;
+
+            GameObject.Find("FPSController").GetComponent<FPSShooter>().enabled = false;
+
+            Time.timeScale = 0;
+        }
     }
 }
